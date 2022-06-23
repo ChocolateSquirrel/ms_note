b02f4d5262fe1d934ce3263222e1676758439110
Marie <marie@exemple.fr>
2022-06-23 17:58:31 +0200

    First commit

---
+++ src/main/java/com/mediscreen/ms_note/MsNoteApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.ms_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediscreen/ms_note/MsNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.ms_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
