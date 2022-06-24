468f57a5c0bd55f8d29fa0917960f78d9e058024
Marie <marie@exemple.fr>
2022-06-24 14:04:56 +0200

    Connect Database Add model

---
+++ src/main/java/com/mediscreen/ms_note/model/Note.java
@@ -0,0 +1,36 @@
+package com.mediscreen.ms_note.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "notes")
+public class Note {
+    @Id
+    private String id;
+    private String patId;
+    private String recommendations;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPatientId() {
+        return patId;
+    }
+
+    public void setPatientId(String patId) {
+        this.patId = patId;
+    }
+
+    public String getRecommendations() {
+        return recommendations;
+    }
+
+    public void setRecommendations(String recommendations) {
+        this.recommendations = recommendations;
+    }
+}
